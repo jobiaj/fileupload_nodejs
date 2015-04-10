963e04ab563b3f48dc56ad34cd786207bc86e0b3
jobiaj <jobyalungal@gmail.com>
2015-04-10 07:13:17 +0000

    first commit

---
+++ Server.js
@@ -0,0 +1,37 @@
+var express=require("express");
+var multer  = require('multer');
+var app=express();
+var done=false;
+
+/*Configure the multer.*/
+
+app.use(multer({ dest: './uploads/',
+ rename: function (fieldname, filename) {
+    return filename+Date.now();
+  },
+onFileUploadStart: function (file) {
+  console.log(file.originalname + ' is starting ...')
+},
+onFileUploadComplete: function (file) {
+  console.log(file.fieldname + ' uploaded to  ' + file.path)
+  done=true;
+}
+}));
+
+/*Handling routes.*/
+
+app.get('/',function(req,res){
+      res.sendfile("index.html");
+});
+
+app.post('/api/photo',function(req,res){
+  if(done==true){
+    console.log(req.files);
+    res.end("File uploaded.");
+  }
+});
+
+/*Run the server.*/
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
